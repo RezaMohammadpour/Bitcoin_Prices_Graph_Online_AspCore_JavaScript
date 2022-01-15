3526a3243359b02a1eadacbe9e288d350a8bde4d
AsreDansh Co <AsreDansh Co@DESKTOP-SSUTJLI>
2022-01-15 23:21:35 +0330

    Add project files.

---
+++ MohammadpourBitCoinShowBitCoinPricesGraph_AspCore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MohammadpourBitCoinShowBitCoinPricesGraph_AspCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MohammadpourBitCoinShowBitCoinPricesGraph_AspCore.Controllers
+{
+    //Developed By Dr.R.Mohammadpour
+    //Reza.Mohammadpour@gmail.com  0098-9177117407
+
+    public class HomeController : Controller
+    {
+        static GraphData graphData = new GraphData();
+        static List<GraphData> listGraphData = new List<GraphData> { graphData};
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetCurrencyGraphData()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                Result result = Newtonsoft.Json.JsonConvert.DeserializeObject<Result>(jsonResponse);
+                graphData.name = "BTC Prices";
+                graphData.x.Add(DateTime.Now.ToLongTimeString().Replace(" PM","").Replace(" AM",""));
+                graphData.y.Add(result.BTC.USD);
+            }
+            return Json(Newtonsoft.Json.JsonConvert.SerializeObject(listGraphData));
+        }
+
+    }
+}
---
+++ MohammadpourBitCoinShowBitCoinPricesGraph_AspCore/Models/BTC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MohammadpourBitCoinShowBitCoinPricesGraph_AspCore.Models
+{
+    public class BTC
+    {
+        public double USD { get; set; }
+    }
+
+    public class Result
+    {
+        public BTC BTC { get; set; }
+    }
+
+    public class GraphData
+    {
+        public GraphData()
+        {
+            x = new List<string>();
+            y = new List<double>();
+            type = "scatter";
+        }
+        public string name { get; set; }
+        public string type { get; set; }
+        public List<string> x { get; set; }
+        public List<double> y { get; set; }
+    }
+}
